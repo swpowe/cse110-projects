5c2db603dafe1582f12f5b0574480c80da996166
Spencer Powell <spencerp@Spencers-Mac-Studio.local>
2024-05-01 19:22:50 -0600

    worked on the team assignment by myself

---
+++ week2-team.py
@@ -0,0 +1,11 @@
+lenSquare = int(input("What is the length of a side of the square? "))
+print(f"The area of the square is: {lenSquare * lenSquare :.1f}")
+
+lenRect = int(input("What is the length of the rectangle?"))
+widthRect = int(input("What is the width of the rectangle?"))
+print(f"The are of the rectangle is: {lenRect * widthRect :.1f}")
+
+pie = 3.14
+radius = float(input("What is the radius of the circle? "))
+print(f"The area of the circle is: {pie * (radius * radius)}")
+
