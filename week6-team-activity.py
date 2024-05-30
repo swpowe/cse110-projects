c8fc0f4e56905baf1dad6aa5ced4ce4b6d35c6b0
swpowe <spencer.powell@me.com>
2024-05-29 18:59:03 -0600

    added some strech goals to activity 6

--- week6-team-activity.py
+++ week6-team-activity.py
@@ -4,5 +4,16 @@ with open("files/hr_system.txt") as raw_data:
     for line in raw_data:
         data_set.append(line.strip())
         info = line.split(" ")
-        print(f"Name: {info[0]} Title: {info[2]}")
+        
+        name = info[0]
+        id = info[1]
+        title = info[2]
+        yearly_salary = float(info[3])
+        
+        paycheck_salary = yearly_salary /12 /2
+        
+        if title == "Engineer":
+            paycheck_salary += 1000
+        
+        print(f"Name: {name} (ID: {id}) Title: {title} - Salary: ${paycheck_salary:.2f}")
         
